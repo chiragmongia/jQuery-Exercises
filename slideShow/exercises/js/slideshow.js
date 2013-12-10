1cedb9554f335343c72c7cdbe1b01624e553f904
Chirag Mongia <chirag.mongia@vinsol.com>
2013-12-11 03:11:12 +0530

    added document.ready(), grouped functions, removed highlightFirstSlide(), changed object to find visible slide

--- slideShow/exercises/js/slideshow.js
+++ slideShow/exercises/js/slideshow.js
@@ -1,13 +1,23 @@
 var SlideShow = function() {
+  this.slideShowBlock = $('#slideshow');
+  this.slideContainer = $('.slide-container');
   this.init();
 }
 
 SlideShow.prototype = {
   init: function() {
+    this.setUpPageForSlideShow();
+    this.startSlideShow();
+  },
+
+  setUpPageForSlideShow: function() {
     this.moveSlidesBlockOnTop();
     this.createImageNumberParagraph();
-    this.highlightFirstSlideNumber();
-    setInterval(this.displaySlides, 4000);
+  },
+
+  startSlideShow: function() {
+    $('#image-number-1').addClass('currentSlide');
+    setInterval(this.displaySlides, 4000);  
   },
 
   displaySlides: function() {
@@ -16,34 +26,31 @@ SlideShow.prototype = {
     }, 1000, function() {
       $(this).appendTo($('#slideshow'));
       $(this).animate({ 'opacity':1 }, 1000);
-      $('#' + $('.slide-container').attr('data-slide-number')).addClass('currentSlide');
+      $('#image-number-' + $('.slide-container:visible').attr('data-slide-number')).addClass('currentSlide');
     });
 
-    $('#' + $('.slide-container').attr('data-slide-number')).removeClass('currentSlide');
+    $('#image-number-' + $('.slide-container:visible').attr('data-slide-number')).removeClass('currentSlide');
   },
 
   createImageNumberParagraph: function() {
     var $numElement = $('<p class="currentImageElement">Image Number: </p>');
-    $numElement.insertAfter('#slideshow');
+    $numElement.insertAfter(this.slideShowBlock);
     this.createImageNumbers($numElement);    
   },
 
   moveSlidesBlockOnTop: function() {
-    $('#slideshow').insertBefore('#header');
+    this.slideShowBlock.insertBefore('#header');
   },
 
   // Creating Image Numbers for the total number of slides
   createImageNumbers: function(imageNumberParagraph) {
-    $('.slide-container').each( function(index) { 
-      var imageNumber = $('<span class="imageNumber"></span>').attr('id', index+1);
+    this.slideContainer.each( function(index) { 
+      var imageNumber = $('<span />', { id: ('image-number-' + (index+1)), text: (index+1), class: 'imageNumber' });
       imageNumber.appendTo(imageNumberParagraph);
-      $('#' + (index+1)).text(index+1);
     });
   },
-
-  highlightFirstSlideNumber: function() {
-    $('#1').addClass('currentSlide');
-  }
 }
 
-var slideshow = new SlideShow();
+$(document).ready(function() {
+  var slideshow = new SlideShow();
+})
